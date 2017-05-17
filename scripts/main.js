70f846ee75d5c54d10738fbd288fd64543ab3fab
Possardt <rpossardt@gmail.com>
2017-05-17 16:34:50 -0400

    added knockout, started styling places bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,22 @@
 var map;
+var pizzaPlaces = [
+    {title: 'Randy\'s',
+    position: {lat: 41.7656821, lng : -72.7151063},
+    id: 'RandysOne' }
+];
+
 function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.7656821, lng : -72.7151063},
         zoom: 11
     });
-
-    var marker = new google.maps.Marker({
-        map: map,
-        position: {lat: 41.7956603, lng: -72.5491068},
-        title: "Randy's Wooster St. Pizza",
-        animation: google.maps.Animation.BOUNCE,
-        id: "Randy's"
-    });
+    for(var i = 0; i < pizzaPlaces.length; i++){
+        new google.maps.Marker({
+            map: map,
+            position: pizzaPlaces[i].position,
+            title: pizzaPlaces[i].title,
+            animation: google.maps.Animation.DROP,
+            id: pizzaPlaces[i].id
+        });
+    }
 }
