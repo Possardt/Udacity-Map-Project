0e0029fcea40aea46a7b54f62ac0d6a13d1ab4f6
Possardt <rpossardt@gmail.com>
2017-05-18 20:52:15 -0400

    started using ko observable array

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 var map;
 var pizzaPlaces = [
-    {title: 'Randy\'s',
+    {name: 'Randy\'s',
     position: {lat: 41.7656821, lng : -72.7151063},
-    id: 'RandysOne' }
+    id: 'RandysOne',
+    stars: 3 }
 ];
 
 function initMap(){
@@ -10,13 +11,30 @@ function initMap(){
         center: {lat: 41.7656821, lng : -72.7151063},
         zoom: 11
     });
+
     for(var i = 0; i < pizzaPlaces.length; i++){
         new google.maps.Marker({
             map: map,
             position: pizzaPlaces[i].position,
-            title: pizzaPlaces[i].title,
+            title: pizzaPlaces[i].name,
             animation: google.maps.Animation.DROP,
             id: pizzaPlaces[i].id
         });
     }
 }
+
+function pizzaPlace(name, stars){
+    var self = this;
+    self.name = ko.observable(name);
+    self.stars = ko.observable(stars);
+};
+
+function PizzaMapViewModel() {
+    var self = this;
+
+    self.pizzaPlaces = ko.observableArray([
+        new pizzaPlace(pizzaPlaces[0].name, pizzaPlaces[0].stars)
+    ]);
+}
+
+ko.applyBindings(new PizzaMapViewModel());
